944e057ca7208c0ad576bec9d922c3bc4c0cd964
driden <lignacio.loureiro@gmail.com>
2020-08-10 20:04:58 -0300

    Need a base page for sharing the driver!

---
+++ AliExpress.PageObject/BasePage.cs
@@ -0,0 +1,14 @@
+﻿namespace AliExpress.PageObject
+{
+    using Interfaces;
+
+    public abstract class BasePage
+    {
+        protected IDriver Driver { get; private set; }
+
+        protected BasePage(IDriver driver)
+        {
+             this.Driver = driver;
+        }
+    }
+}
