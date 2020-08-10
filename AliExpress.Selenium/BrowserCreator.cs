682081be5e18e862433f1ab4cbe682907a746a47
driden <lignacio.loureiro@gmail.com>
2020-08-10 20:06:44 -0300

    Need to have a member in charge of creating a driver

---
+++ AliExpress.Selenium/BrowserCreator.cs
@@ -0,0 +1,9 @@
+﻿namespace AliExpress.Selenium
+{
+    using PageObject.Interfaces;
+
+    public abstract class BrowserCreator
+    {
+        public abstract IDriver CreateChromeDriver();
+    }
+}
---
+++ AliExpress.Selenium/ChromeCreator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AliExpress.Selenium
+{
+    using PageObject.Interfaces;
+
+    public class ChromeCreator: BrowserCreator
+    {
+        public override IDriver CreateChromeDriver()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
